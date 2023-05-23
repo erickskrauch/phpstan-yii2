33981f4b6c05ef1e68d81ccbc8f707feea5abe55
ErickSkrauch <erickskrauch@yandex.ru>
2023-05-23 02:40:05 +0200

    Cover Controller::actions() type

---
+++ tests/Stubs/Controller.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace ErickSkrauch\PHPStan\Yii2\Tests\Stubs;
+
+use yii\base\Controller as Yii2Controller;
+use yii\web\ViewAction;
+
+final class Controller extends Yii2Controller {
+
+    public function actions(): array {
+        return [
+            'action1' => ViewAction::class,
+            'action2' => [
+                'class' => ViewAction::class,
+                'viewParam' => 'myParam',
+            ],
+            'action3' => [
+                '__class' => ViewAction::class,
+                'defaultView' => 'default',
+            ],
+            'action4' => fn() => new ViewAction('action4', $this),
+        ];
+    }
+
+}
