5bfa6f38bd86e1b261d45f9ef3e4dc0a1e2511e0
ErickSkrauch <erickskrauch@yandex.ru>
2023-05-23 02:30:30 +0200

    Cover Component::behaviors() type

---
+++ tests/Stubs/Component.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace ErickSkrauch\PHPStan\Yii2\Tests\Stubs;
+
+use yii\base\Component as Yii2Component;
+use yii\behaviors\TimestampBehavior;
+
+final class Component extends Yii2Component {
+
+    public function behaviors(): array {
+        return [
+            new TimestampBehavior(),
+            TimestampBehavior::class,
+            [
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => 'createdAt',
+            ],
+            [
+                '__class' => TimestampBehavior::class,
+                'updatedAtAttribute' => 'updatedAt',
+            ],
+            fn() => new TimestampBehavior(),
+        ];
+    }
+
+}
